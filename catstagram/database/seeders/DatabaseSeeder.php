d675bfc8f3eed1b3e895126acd9e3541ade22b4f
petritg <petrit.gashi@student.ehb.be>
2024-01-11 17:15:28 +0100

    html en factory

--- catstagram/database/seeders/DatabaseSeeder.php
+++ catstagram/database/seeders/DatabaseSeeder.php
@@ -15,21 +15,23 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
-        Post::create([
-            'title' => 'Schattige Kat',
-            'tags' => 'schattig, kitten, grijs',
-            'location' => 'Brussel',
-            'breed' => 'Burmilla',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt elementum turpis, vitae ultrices tortor luctus sed. Sed sed erat eu enim eleifend luctus nec et elit. Fusce porta commodo tempor.'
-        ]);
+        Post::factory(6)->create();
 
-        Post::create([
-            'title' => 'Slaperige Kat',
-            'tags' => 'slaperig, gevlekt',
-            'location' => 'New York',
-            'breed' => 'Ragdoll',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt elementum turpis, vitae ultrices tortor luctus sed. Sed sed erat eu enim eleifend luctus nec et elit. Fusce porta commodo tempor.'
-        ]);
+        // Post::create([
+        //     'title' => 'Schattige Kat',
+        //     'tags' => 'schattig, kitten, grijs',
+        //     'location' => 'Brussel',
+        //     'breed' => 'Burmilla',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt elementum turpis, vitae ultrices tortor luctus sed. Sed sed erat eu enim eleifend luctus nec et elit. Fusce porta commodo tempor.'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Slaperige Kat',
+        //     'tags' => 'slaperig, gevlekt',
+        //     'location' => 'New York',
+        //     'breed' => 'Ragdoll',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt elementum turpis, vitae ultrices tortor luctus sed. Sed sed erat eu enim eleifend luctus nec et elit. Fusce porta commodo tempor.'
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- catstagram/resources/views/post.blade.php
+++ catstagram/resources/views/post.blade.php
@@ -1,2 +1,10 @@
+@extends('layout')
+
+@section('content')
+    
+
+
 <h2>{{$post['title']}}</h2>
 <p>{{$post['description']}}</p>
+
+@endsection
