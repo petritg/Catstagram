67787e0ba5d00e2f3f299ee7b06cd5e171bd6d41
petritg <petrit.gashi@student.ehb.be>
2024-01-11 22:03:46 +0100

    ander logo

--- catstagram/resources/views/components/layout.blade.php
+++ catstagram/resources/views/components/layout.blade.php
@@ -26,7 +26,7 @@
                     },
                 };
             </script>
-            <title>LaraGigs | Find Laravel Jobs & Projects</title>
+            <title>Catstagram</title>
         </head>
         <body class="mb-48">
             <nav class="flex justify-between items-center mb-4">
--- catstagram/resources/views/partials/_hero.blade.php
+++ catstagram/resources/views/partials/_hero.blade.php
@@ -8,10 +8,10 @@
 
             <div class="z-10">
                 <h1 class="text-6xl font-bold uppercase text-white">
-                    Lara<span class="text-black">Gigs</span>
+                    Catsta<span class="text-black">Gram</span>
                 </h1>
                 <p class="text-2xl text-gray-200 font-bold my-4">
-                    Find or post Laravel jobs & projects
+                    Alleen maar katten
                 </p>
                 <div>
                     <a
