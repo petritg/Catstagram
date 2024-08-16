ae96ad86bfee2070f0f439fe7f38f47f2e132c97
petritg <petrit.gashi@student.ehb.be>
2024-08-16 19:39:29 +0200

    index faq

---
+++ catstagram/resources/views/faq/index.blade.php
@@ -0,0 +1,26 @@
+<!-- resources/views/faqs/index.blade.php -->
+
+<x-layout>
+    <h1 class="text-2xl font-bold mb-4">FAQ Management</h1>
+
+    <a href="{{ route('faqs.create') }}" class="text-blue-500 hover:underline">Create New FAQ</a>
+
+    @if (session('success'))
+        <div class="text-green-500">{{ session('success') }}</div>
+    @endif
+
+    <ul class="mt-6">
+        @foreach ($faqs as $faq)
+            <li class="mb-4">
+                <h2 class="text-xl font-semibold">{{ $faq->question }}</h2>
+                <p>{{ $faq->answer }}</p>
+                <a href="{{ route('faqs.edit', $faq->id) }}" class="text-blue-500 hover:underline">Edit</a>
+                <form action="{{ route('faqs.destroy', $faq->id) }}" method="POST" class="inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-red-500 hover:underline">Delete</button>
+                </form>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
