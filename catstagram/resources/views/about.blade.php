139f9446d490d33f894b40422d21ed2b93ea8b32
petritg <petrit.gashi@student.ehb.be>
2024-08-19 20:54:56 +0200

    about restyling

--- catstagram/resources/views/about.blade.php
+++ catstagram/resources/views/about.blade.php
@@ -3,5 +3,6 @@
     <p>Voor dit project heb ik mij gebaseerd op de 4-uur lange <a href="https://youtu.be/MYyJ4PuL4pY?si=l9Ex5aoWbdLoWNyV">video</a> van 
         het Youtube Kanaal Traversy Media </p>
     <br>
-    <a href="https://github.com/petritg/Catstagram.git">Mijn GitHub repo</a>
+    <a href="https://youtu.be/MYyJ4PuL4pY?si=l9Ex5aoWbdLoWNyV" target="_blank" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-300">Video link</a>
+    <a href="https://github.com/petritg/Catstagram.git" target="_blank" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-300">Mijn GitHub repo</a>
 </x-layout>
